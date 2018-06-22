e681c752229cc0064d87990a39b48f4845ad581e
mahendra_singh <mahendra_singh@seologistics.com>
2018-06-22 18:35:34 +0530

    Create website for country flags

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# country-flags
+# Country flags of the world
+National flags icons (SVG and PNG)  and JSON file of the all 245 independent countries of the world in alphabetical order
+# Live URL 
+https://mahendralife.github.io/angular2-material-icons/
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { StoresService } from '../services/stores.service';
+import { HttpClient } from '@angular/common/http';
+import { countryList } from '../flag-data/countries';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  country = Object.keys(countryList);
+  countryList = countryList;
+
+  isActiveItem: String = null;
+  searchByIconName: String;
+
+
+  constructor(private store: StoresService, private http: HttpClient ) { }
+    getIcon(icon) {
+      return `../../assets/flag-icons/png/${icon.toLocaleLowerCase()}.png`;
+    }
+  activeItem (icon, index) {
+    this.isActiveItem = icon;
+  }
+
+
+  ngOnInit() {
+
+    this.store.searchText.subscribe(str => {
+      this.searchByIconName = str ? str : '' ;
+    });
+  }
+
+}
