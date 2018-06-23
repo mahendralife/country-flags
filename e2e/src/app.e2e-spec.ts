c46ee45c9c23d2d46aca3b1a5a3db6b5e7e38d25
Mahnedra Singh <mahendra.s.life@gmail.com>
2018-06-24 00:32:26 +0530

    change workspace

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular2-material-icons!');
+    expect(page.getParagraphText()).toEqual('Welcome to country flags');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular2-material-icons!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to country flag!');
   }));
 });
